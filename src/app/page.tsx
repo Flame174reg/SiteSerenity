2f6291e99e4e54eda138e02d2e12b767c4a7c855
Flame174reg <153459605+Flame174reg@users.noreply.github.com>
2025-08-20 08:56:26 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
-import Image from "next/image";
 
 export default function Home() {
   const { data: session, status } = useSession();
