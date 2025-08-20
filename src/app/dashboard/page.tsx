fe9102a6a3e465a26fe70d8f58fff9835d754649
Flame174reg <153459605+Flame174reg@users.noreply.github.com>
2025-08-20 06:36:43 +0500

    Add files via upload

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">Добро пожаловать в Dashboard 🚀</h1>
+      <p className="text-gray-600 mt-2">
+        Это ваша первая отдельная страница на сайте.
+      </p>
+    </main>
+  );
+}
