e9c9edec4ed874b71d8e0f9b7b4f7735f983a79c
Flame174reg <153459605+Flame174reg@users.noreply.github.com>
2025-08-20 07:01:29 +0500

    Create route.ts

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Discord from "next-auth/providers/discord";
+
+const handler = NextAuth({
+  providers: [
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      authorization: { params: { scope: "identify guilds email" } },
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
