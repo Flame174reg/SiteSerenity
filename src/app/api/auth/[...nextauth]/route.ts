09826229aa818f747177ad5c116217302c3a5536
Flame174reg <153459605+Flame174reg@users.noreply.github.com>
2025-08-23 00:24:59 +0500

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,2 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import { handlers } from "../../../../auth";
-
-// ВАЖНО: Никаких других export в этом файле!
-export const { GET, POST } = handlers;
+export { GET, POST } from "@/auth";
