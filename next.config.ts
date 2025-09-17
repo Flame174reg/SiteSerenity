00ee23c4cf1758a3fc041a42bec06df68be42fed
Flame174reg <153459605+Flame174reg@users.noreply.github.com>
2025-09-17 13:36:49 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.public.blob.vercel-storage.com" },
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
